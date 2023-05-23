97b84ad38ed8a01e37eeff44c2f418100a0ad461
ZawadAhmed <zawadwasik@gmail.com>
2023-05-24 03:39:21 +0800

    Wallet Connection

--- pages/test.js
+++ pages/test.js
@@ -1,9 +1,71 @@
 import { Button, Container } from '@nextui-org/react';
-import Menubar from "../components/MenuBar"
-import metamask from "../ethereum/metamask"
+import Menubar from "../component/MenuBar"
+
+import { useEffect, useState } from "react";
+
 
 const Test = () => {
-    return (
+  const [walletAddress, setWalletAddress] = useState("");
+
+  useEffect(() => {
+    getCurrentWalletConnected();
+    addWalletListener();
+  }, [walletAddress]);
+
+  const connectWallet = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        /* MetaMask is installed */
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const getCurrentWalletConnected = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        if (accounts.length > 0) {
+          setWalletAddress(accounts[0]);
+          console.log(accounts[0]);
+        } else {
+          console.log("Connect to MetaMask using the Connect button");
+        }
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const addWalletListener = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      });
+    } else {
+      /* MetaMask is not installed */
+      setWalletAddress("");
+      console.log("Please install MetaMask");
+    }
+  };
+  
+  
+  return (
   
         
        
@@ -12,10 +74,21 @@ const Test = () => {
         <h1>Welcome to my website!</h1>
         <p>This is the homepage.</p>
     
-       <Menubar/>
-        
-        <button onClick='enableEthereumButton'> Click me</button>; 
-        
+        <Menubar/>
+
+          <Button
+                className="button is-white connect-wallet"
+                onClick={connectWallet}
+              >
+                <span className="is-link has-text-weight-bold">
+                  {walletAddress && walletAddress.length > 0
+                    ? `Connected: ${walletAddress.substring(
+                        0,
+                        6
+                      )}...${walletAddress.substring(38)}`
+                    : "Connect Wallet"}
+                </span>
+              </Button>   
         
         </Container>
         
